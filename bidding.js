15bceb83288682f6a0f3650fe9cd06306a8973b1
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-02-11 21:24:14 +0530

    Update bidding.js

--- bidding.js
+++ bidding.js
@@ -1,27 +1,68 @@
+function displayResult(message) {
+    document.getElementById("timer").style.display = "none";
+    var result = document.getElementById("result");
+    result.textContent = message;
+    result.style.display = "block";
+    //"Time us up!!!!!!!!!"
+}
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        intervalId = setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+            
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+    display.textContent = minutes + ":" + seconds;
+    
+    if (--timer < 0) {
+        clearInterval(intervalId);
+        displayResult("Time-is-up!!!")
+    }
+  }, 1000);
+  }
+
+var n=1000000
+var i=0
 var intervalId;
 document.getElementById("startTimer").addEventListener("click", function() {
-  var inputValue = document.getElementById("inputBid").value;
-  if (inputValue) {
+//   var inputValue = document.getElementById("inputBid").value;
+//   if (inputValue) {
+    // var re = document.getElementById("newBid");
+    // re.style.display = "inline";  
+    // var r = document.getElementById("startTimer");
+    // r.disabled = isDisabled
+
     clearInterval(intervalId);
     var duration = 300;
     var display = document.querySelector('#timer');
     startTimer(300, display);
-  }
+//   }
 });
-
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  intervalId = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      clearInterval(intervalId);
+document.getElementById("newBid").addEventListener("click", function() {
+var number = document.getElementById("inputBid").value;
+console.log(number);
+res = document.getElementById("result")
+if(res.style.display=="none"){
+    console.log("letss gooo")
+    i=i+1
+    if(i==1){
+        n= number
+        console.log("First")
+        var lo = document.getElementById("lowe");
+        lo.textContent = number;
+        lo.style.display = "block"
     }
-  }, 1000);
+    else{
+        console.log(n)
+        console.log("no")
+        console.log(n)
+        if(number<n){
+        lo = document.getElementById("lowe");
+        lo.clearInterval;
+        lo.textContent = number;
+        }
+    }
 }
+});
